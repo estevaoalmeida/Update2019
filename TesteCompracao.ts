3065094d57b32e7d11e77ef56cc2f09b8a1c0187
Estêvão Almeida <86606638+estevaoalmeida@users.noreply.github.com>
2023-12-01 10:45:55 -0300

    Teste1

--- TesteCompracao.ts
+++ TesteCompracao.ts
@@ -1,129 +1,85 @@
 {
-    "status": "PENDENTE",
-    "dataHoraEmissao": "2023-11-30T21:57:51.89Z",
-    "dataHoraPrevisaoEntrega": "2023-11-30T21:57:51.89Z",
-    "financeiroGerado": false,
-    "produtos": [],
-    "servicos": [
-      {
-        "item": 1,
-        "descricao": "asdasd",
-        "quantidade": 1,
-        "valorCusto": 10,
-        "valorTotal": 10
-      }
-    ],
-    "veiculos": [],
-    "equipamentos": [],
-    "cobrancas": [],
-    "vinculos": [],
-    "valorTotal": 10,
-    "qtdeTotalItensProdutos": 0,
-    "qtdeTotalItensServicos": 1,
-    "_id": "65690565635ecea864783f0d",
-    "idEmpresa": "5fbffe47de093c166cfd7adf",
-    "idTerminal": "5fbffe4fde093c166cfd7b0f",
-    "idCliente": "6511e76df0731c3b8530319b",
-    "idFornecedor": "650467d2c81c13df18c0d371",
-    "idFuncionario": "5fbffe48de093c166cfd7ae4",
-    "dataHoraEntrega": "2023-11-30T23:01:00.365Z",
-    "codigoServicoTerceirizado": "56701381471",
-    "sequencial": 60,
-    "descricao": "asdasdasdS",
-    "segmento": "EQUIPAMENTO",
+  "status": "PENDENTE",
+  "dataHoraEmissao": "2023-11-30T21:57:51.89Z",
+  "dataHoraPrevisaoEntrega": "2023-11-30T21:57:51.89Z",
+  "financeiroGerado": false,
+  "produtos": [],
+  "servicos": [
+    {
+      "item": 1,
+      "descricao": "asdasd",
+      "quantidade": 1,
+      "valorCusto": 10,
+      "valorTotal": 10
+    }
+  ],
+  "veiculos": [],
+  "equipamentos": [],
+  "cobrancas": [],
+  "vinculos": [],
+  "valorTotal": 10,
+  "qtdeTotalItensProdutos": 0,
+  "qtdeTotalItensServicos": 1,
+  "_id": "65690565635ecea864783f0d",
+  "idEmpresa": "5fbffe47de093c166cfd7adf",
+  "idTerminal": "5fbffe4fde093c166cfd7b0f",
+  "idCliente": "6511e76df0731c3b8530319b",
+  "idFornecedor": "650467d2c81c13df18c0d371",
+  "idFuncionario": "5fbffe48de093c166cfd7ae4",
+  "dataHoraEntrega": "2023-11-30T23:01:00.365Z",
+  "codigoServicoTerceirizado": "56701381471",
+  "sequencial": 60,
+  "descricao": "asdasdasdS",
+  "segmento": "EQUIPAMENTO",
+  "inEmpresa": {
+    "razaoSocial": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO",
+    "nomeFantasia": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO"
+  },
+  "inTerminal": {
+    "descricao": "TERMINAL PADRÃO",
     "inEmpresa": {
       "razaoSocial": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO",
       "nomeFantasia": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO"
     },
-    "inTerminal": {
-      "descricao": "TERMINAL PADRÃO",
-      "inEmpresa": {
-        "razaoSocial": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO",
-        "nomeFantasia": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO"
+    "inCaixa": {
+      "descricao": "CAIXA PADRÃO",
+      "tipoOperacao": "CONTÍNUO"
+    }
+  },
+  "inCliente": {
+    "nomeRazaoSocial": "sadasdasasd",
+    "apelidoNomeFantasia": null,
+    "cpfCnpj": "34423211111",
+    "rgIe": null,
+    "codigo": 1858,
+    "endereco": {
+      "tipo": "ENTREGA",
+      "cep": "",
+      "logradouro": "",
+      "numero": "",
+      "complemento": "",
+      "pontoReferencia": null,
+      "idBairro": "64887319fe408e8d32faf701",
+      "idCidade": "5c5bfc4648269d14c1c46469",
+      "principal": true,
+      "inCidade": {
+        "descricao": "Parobé",
+        "uf": "RS",
+        "codigoIbge": "4314050"
       },
-      "inCaixa": {
-        "descricao": "CAIXA PADRÃO",
-        "tipoOperacao": "CONTÍNUO"
+      "inBairro": {
+        "descricao": "MORUNGAVA"
       }
     },
-    "inCliente": {
-      "nomeRazaoSocial": "sadasdasasd",
-      "apelidoNomeFantasia": null,
-      "cpfCnpj": "34423211111",
+    "inEmpresaCadastro": {
+      "razaoSocial": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO",
+      "nomeFantasia": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO"
+    },
+    "inRepresentante": {
+      "nomeRazaoSocial": "FORNECEDORA PRÓPRIA",
+      "apelidoNomeFantasia": "FORNECEDORA PRÓPRIA",
+      "cpfCnpj": null,
       "rgIe": null,
-      "codigo": 1858,
-      "endereco": {
-        "tipo": "ENTREGA",
-        "cep": "",
-        "logradouro": "",
-        "numero": "",
-        "complemento": "",
-        "pontoReferencia": null,
-        "idBairro": "64887319fe408e8d32faf701",
-        "idCidade": "5c5bfc4648269d14c1c46469",
-        "principal": true,
-        "inCidade": {
-          "descricao": "Parobé",
-          "uf": "RS",
-          "codigoIbge": "4314050"
-        },
-        "inBairro": {
-          "descricao": "MORUNGAVA"
-        }
-      },
-      "inEmpresaCadastro": {
-        "razaoSocial": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO",
-        "nomeFantasia": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO"
-      },
-      "inRepresentante": {
-        "nomeRazaoSocial": "FORNECEDORA PRÓPRIA",
-        "apelidoNomeFantasia": "FORNECEDORA PRÓPRIA",
-        "cpfCnpj": null,
-        "rgIe": null,
-        "identificadorIntermediario": null,
-        "inEmpresaCadastro": {
-          "razaoSocial": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO",
-          "nomeFantasia": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO"
-        },
-        "inGrupoFornecedor": {
-          "descricao": "GERAL",
-          "nomeFluxoCaixa": "LUCRO LÍQUIDO",
-          "tipoFluxoCaixa": "ANALÍTICA"
-        },
-        "inPlanoContas": null
-      },
-      "inTransportadora": null,
-      "inGrupoCliente": {
-        "_id": "608178ef6d704741f5773c24",
-        "descricao": "GRUPO TESTE"
-      },
-      "inEspeciePagamento": {
-        "descricao": "DINHEIRO",
-        "tipoLancamento": "01",
-        "exigeNumeroAutorizacao": true,
-        "debitoPreDatado": false
-      },
-      "inCondicaoPagamento": {
-        "descricao": "A VISTA",
-        "numeroParcelas": 1
-      },
-      "inTabelaPreco": {
-        "descricao": "TABELA PADRÃO"
-      },
-      "inVendedor": {
-        "nome": "Leo",
-        "inEmpresaCadastro": {
-          "razaoSocial": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO",
-          "nomeFantasia": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO"
-        },
-        "inUsuario": null
-      }
-    },
-    "inFornecedor": {
-      "nomeRazaoSocial": "ELITE COMERCIO DE AREIA LTDA",
-      "apelidoNomeFantasia": "ARGAMASSA RAMOS",
-      "cpfCnpj": "11408746000155",
-      "rgIe": "0430156340",
       "identificadorIntermediario": null,
       "inEmpresaCadastro": {
         "razaoSocial": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO",
@@ -136,35 +92,79 @@
       },
       "inPlanoContas": null
     },
-    "inFuncionario": {
-      "nome": "ESTEVAO ALMEIDA",
+    "inTransportadora": null,
+    "inGrupoCliente": {
+      "_id": "608178ef6d704741f5773c24",
+      "descricao": "GRUPO TESTE"
+    },
+    "inEspeciePagamento": {
+      "descricao": "DINHEIRO",
+      "tipoLancamento": "01",
+      "exigeNumeroAutorizacao": true,
+      "debitoPreDatado": false
+    },
+    "inCondicaoPagamento": {
+      "descricao": "A VISTA",
+      "numeroParcelas": 1
+    },
+    "inTabelaPreco": {
+      "descricao": "TABELA PADRÃO"
+    },
+    "inVendedor": {
+      "nome": "Leo",
       "inEmpresaCadastro": {
         "razaoSocial": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO",
         "nomeFantasia": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO"
       },
-      "inUsuario": {
-        "idUsuario": "5fbffe45de093c166cfd7ab3",
-        "nome": "Estevao",
-        "email": "estevao@ibrsistemas.com.br",
-        "inGrupoUsuario": {
-          "descricao": "ADMINISTRAÇÃO"
-        }
+      "inUsuario": null
+    }
+  },
+  "inFornecedor": {
+    "nomeRazaoSocial": "ELITE COMERCIO DE AREIA LTDA",
+    "apelidoNomeFantasia": "ARGAMASSA RAMOS",
+    "cpfCnpj": "11408746000155",
+    "rgIe": "0430156340",
+    "identificadorIntermediario": null,
+    "inEmpresaCadastro": {
+      "razaoSocial": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO",
+      "nomeFantasia": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO"
+    },
+    "inGrupoFornecedor": {
+      "descricao": "GERAL",
+      "nomeFluxoCaixa": "LUCRO LÍQUIDO",
+      "tipoFluxoCaixa": "ANALÍTICA"
+    },
+    "inPlanoContas": null
+  },
+  "inFuncionario": {
+    "nome": "ESTEVAO ALMEIDA",
+    "inEmpresaCadastro": {
+      "razaoSocial": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO",
+      "nomeFantasia": "EMPRESA ESTEVAO ALMEIDA AMBIENTE DE HOMOLOGACAO"
+    },
+    "inUsuario": {
+      "idUsuario": "5fbffe45de093c166cfd7ab3",
+      "nome": "Estevao",
+      "email": "estevao@ibrsistemas.com.br",
+      "inGrupoUsuario": {
+        "descricao": "ADMINISTRAÇÃO"
       }
-    },
-    "tokenOrganizacao": null,
-    "dispositivos": [],
-    "dispositivoCriacao": {
-      "identityToken": null,
-      "tipoAcesso": "WEB",
-      "identificadorDispositivo": null,
-      "versao": null,
-      "logado": false,
-      "bloqueado": false,
-      "createdAt": "2020-11-26T19:13:09.279Z",
-      "ultimoLogin": "2023-11-30T21:53:21.268Z"
-    },
-    "createdAt": "2023-11-30T21:57:57.361Z",
-    "updatedAt": "2023-11-30T23:01:19.402Z",
-    "deletedAt": "0001-01-01T00:00:00Z",
-    "isDeleted": false
-  }
+    }
+  },
+  "tokenOrganizacao": null,
+  "dispositivos": [],
+  "dispositivoCriacao": {
+    "identityToken": null,
+    "tipoAcesso": "WEB",
+    "identificadorDispositivo": null,
+    "versao": null,
+    "logado": false,
+    "bloqueado": false,
+    "createdAt": "2020-11-26T19:13:09.279Z",
+    "ultimoLogin": "2023-11-30T21:53:21.268Z"
+  },
+  "createdAt": "2023-11-30T21:57:57.361Z",
+  "updatedAt": "2023-11-30T23:01:19.402Z",
+  "deletedAt": "0001-01-01T00:00:00Z",
+  "isDeleted": false
+}
